cf7910545a3df665943a5c6d54258385bf3eab1f
jcarnaggio <jordancarnaggio@gmail.com>
2015-05-10 11:22:32 -0700

    Updated sorts
    Added the time sort and added some finishing touches. Complete for the
    most part but may require maintenance because I haven't been able to
    test them.

--- sort_data.cpp
+++ sort_data.cpp
@@ -32,12 +32,63 @@ vector<baseball_team*> sort_data::of_this_league(major_league league)
 
 vector<baseball_team*> sort_data::sort_team_names(vector<baseball_team*> baseball_teams)
 {
-    // MSD sort goes here
+    sort(baseball_teams.begin(), baseball_teams.end(), [] (const baseball_team& x, const baseball_team& y) { return x.get_team_name() < y.get_team_name();});
+
+    return baseball_teams;
 }
 
 vector<baseball_team*> sort_data::sort_stadium_names(vector<baseball_team *> baseball_teams)
 {
-    // MSD sort goes here
+    sort(baseball_teams.begin(), baseball_teams.end(), [] (const baseball_team& x, const baseball_team& y) { return x.get_stadium_name() < y.get_stadium_name();});
+
+    return baseball_teams;
+}
+
+vector<baseball_team*> sort_data::sort_dates(vector<baseball_team*> baseball_teams)
+{
+    int i, j, key, len = baseball_teams.length();
+
+    for(j = 1; j < len; j++)
+    {
+        key = baseball_teams.at(j)->get_day();
+
+        for(i = j - 1; (i >= 0) && (baseball_teams.at(i)->get_day() < key); i--)
+        {
+            baseball_teams.at(i + 1) = baseball_teams.at(i);
+        }
+
+        baseball_teams.at(i + 1)->get_day() = key;
+    }
+
+    i, j, key, len = baseball_teams.length();
+
+    for(j = 1; j < len; j++)
+    {
+        key = baseball_teams.at(j)->get_month();
+
+        for(i = j - 1; (i >= 0) && (baseball_teams.at(i)->get_month() < key); i--)
+        {
+            baseball_teams.at(i + 1) = baseball_teams.at(i);
+        }
+
+        baseball_teams.at(i + 1)->get_month() = key;
+    }
+
+    i, j, key, len = baseball_teams.length();
+
+    for(j = 1; j < len; j++)
+    {
+        key = baseball_teams.at(j)->get_year();
+
+        for(i = j - 1; (i >= 0) && (baseball_teams.at(i)->get_year() < key); i--)
+        {
+            baseball_teams.at(i + 1) = baseball_teams.at(i);
+        }
+
+        baseball_teams.at(i + 1)->get_year() = key;
+    }
+
+    return baseball_teams;
 }
 
 vector<baseball_team*> sort_data::by_team_name(major_league league)
@@ -68,4 +119,11 @@ vector<baseball_team*> sort_data::by_grass_surface(major_league league)
     }
 
     return sort_team_names(temp2);
+}
+
+vector<baseball_team*> sort_data::by_date_opened(major_league league)
+{
+    vector<baseball_team*> temp = of_this_league(league);
+
+    return sort_dates(temp);
 }
--- sort_data.h
+++ sort_data.h
@@ -12,6 +12,7 @@ private:
     vector<baseball_team*> of_this_league    (major_league league);
     vector<baseball_team*> sort_team_names   (vector<baseball_team*> baseball_teams);
     vector<baseball_team*> sort_stadium_names(vector<baseball_team*> baseball_teams);
+    vector<baseball_team*> sort_dates        (vector<baseball_team*> baseball_teams);
 public:
      sort_data(vector<baseball_team*> baseball_teams);
     ~sort_data();
@@ -19,6 +20,7 @@ public:
     vector<baseball_team*> by_team_name    (major_league league);
     vector<baseball_team*> by_stadium_name (major_league league);
     vector<baseball_team*> by_grass_surface(major_league league);
+    vector<baseball_team*> by_date_opened  (major_league league);
 };
 
 #endif
