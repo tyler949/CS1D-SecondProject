41e85277bc9b44d1e5fcd675f54fa1881fd49f42
tyler949 <tyler949@yahoo.com>
2015-05-11 09:41:13 -0700

    zzz
    zzz

---
+++ main.cpp
@@ -0,0 +1,13 @@
+
+
+#include <iostream>
+#include <QVector>
+#include "baseball_team_class.h"
+#include "souvenir_class.h"
+
+int main() {
+    QVector<baseball_team> myTeams;
+    QVector<souvenirs> mySouvenirs;
+    
+    return 0;
+}
